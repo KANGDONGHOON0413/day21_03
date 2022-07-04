acf37d7680a33d39d4fea67b5785a225ce138bfc
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-04 12:44:30 +0900

    초기 커밋.

---
+++ day21_03/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace day21_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ day21_03/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day21_03
+{   
+    public partial class Form1 : Form
+    {
+        int  SIZ= 300;
+        Rectangle[] Rect;
+        public Form1()
+        {
+            InitializeComponent();
+            Rect = new Rectangle[100];
+            Width = SIZ;
+            Height = SIZ;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            for( int i = 0; i <Rect.Length; i++)
+            {
+                Rect[i].X = rand.Next(200);
+                Rect[i].Y = rand.Next(200);
+                Rect[i].Width = 60;
+                Rect[i].Height = 60;
+            }
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < Rect.Length; i++) e.Graphics.DrawRectangle(Pens.Black, Rect[i]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("pause");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Control ctrl = (Control)sender;
+
+            ctrl.Width = SIZ;
+            ctrl.Height = SIZ;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Logout?", "quit", MessageBoxButtons.YesNo) == DialogResult.No) e.Cancel = true;
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Logout success");
+        }
+    }
+}
